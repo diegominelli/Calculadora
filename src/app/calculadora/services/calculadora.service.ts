cbf3c827f5ff625fee3cb0631ae0f48cc35f43a5
diegominelli <diego.minelli@hotmail.com>
2020-09-17 20:34:57 -0300

    Documentando as variaveis e as operações no serviço CalculadoraService

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+  
 export class CalculadoraService {
 
+  /*Define as constantes utilizadas
+    para definir as operações de cálculo */
+
+  static readonly SOMA: string = '+';  
+  static readonly SUBSTRAÇÃO: string = '-';  
+  static readonly DIVISÃO: string = '/';  
+  static readonly MULTIPLICAÇÃO: string = '*';  
+
   constructor() { }
+
+  /** 
+   * Método que calcula uma operação matemática dado
+   * dois números
+   * Suporta as operações soma, subtração, divisão
+   * e multiplicação.
+   * 
+   *@param num1 number
+   *@param num2 number
+   *@param operacao string Operação a ser executada
+   *@return number Resultado da operação 
+  */ 
+
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number; //armazena o resultado da operação 
+
+    switch(operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBSTRAÇÃO:
+        resultado = num1 - num2;
+      break; 
+      case CalculadoraService.DIVISÃO:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICAÇÃO:
+        resultado = num1 * num2;
+      break;
+      default:
+        resultado = 0;
+    }
+    
+    return resultado;
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
